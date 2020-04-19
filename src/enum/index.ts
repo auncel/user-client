0083ac5868a5c9563bd83990b4a35fd0175f1540
yidafu <tiaozuwushi@qq.com>
2020-04-19 21:28:52 +0800

    feat: add enmu file from Java

---
+++ src/enum/index.ts
@@ -0,0 +1,27 @@
+/* --------------------------------------------------------------------------*
+ * Description:                                                              *
+ *                                                                           *
+ * File Created: Sunday, 19th April 2020 10:30 am                            *
+ * Author: yidafu(dov-yih) (me@yidafu.dev)                                   *
+ *                                                                           *
+ * Last Modified: Sunday, 19th April 2020 10:30 am                           *
+ * Modified By: yidafu(dov-yih) (me@yidafu.dev>)                             *
+ *                                                                           *
+ * Copyright 2019 - 2020 Mozilla Public License 2.0                          *
+ *-------------------------------------------------------------------------- */
+
+export enum ProblemDifficulty {
+  Easy,
+  Medium,
+  Hard,
+}
+
+export enum SubmissionStatus {
+  ACCEPT,
+  PADDING,
+  PADDING_TIMEOUT,
+  JUDGING,
+  WRONG_ANWSER,
+  RENDER_ERROR,
+  SYNTAX_ERROR,
+}
